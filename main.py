a00f6a735ba5e3e944e025a72caaa746bb113a1d
Alan Daniels <alan.daniels10@gmail.com>
2024-03-14 20:08:27 +1100

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,60 @@ from pylatex import Math, TikZ, Axis, Plot, Figure, Matrix, Alignat
 from pylatex.utils import italic
 
 import os
+import random
+
+
+# $$C = B\log_2(1+SNR)$$
+def ShannonSolveC(qdoc, adoc, vars, depth):
+    if depth == 0:
+        vars["B"] = RandomB(vars)
+        vars["SNR"] = RandomSNR(vars)
+        qdoc.append(
+            "Given B={} and SNR={};\n Using the ".format(vars["B"], vars["SNR"])
+        )
+        qdoc.append(italic("Shannon Capacity Formula "))
+        qdoc.append(
+            "what is the theoretical maximum capacity?\n".format(
+                vars["B"], vars["SNR"], italic("Shannon Capacity Formula")
+            )
+        )
+    if callable(vars["B"]):
+        vars["B"] = vars["B"](qdoc, adoc, vars, depth + 1)
+    if callable(vars["SNR"]):
+        vars["SNR"] = vars["SNR"](qdoc, adoc, vars, depth + 1)
+
+
+# $$B = f_H-f_L$$
+def SolveB(qdoc, adoc, vars, depth):
+    pass
+
+
+# $$SNR = 10^{(\frac{SNR_{dB}}{10})}$$
+def SolveSNR(qdoc, adoc, vars, depth):
+    pass
+
+
+def RandomB(vars):
+    return 1_000_001
+
+def RandomSNR(vars):
+    return 1.5
+
 
 if __name__ == "__main__":
-    geometry_options = {"tmargin": "1cm", "lmargin": "10cm"}
-    doc = Document(geometry_options=geometry_options)
+    random.seed()
+    geometry_options = {"margin": "1cm"}
+    qdoc = Document(geometry_options=geometry_options)
+    adoc = Document(geometry_options=geometry_options)
 
-    # creating a pdf with title "the simple stuff"
-    with doc.create(Section("The simple stuff")):
-        doc.append("Some regular text and some")
-        doc.append(italic("italic text. "))
-        doc.append("\nAlso some crazy characters: $&#{}")
-        with doc.create(Subsection("Math that is incorrect")):
-            doc.append(Math(data=["2*3", "=", 9]))
-
-        # creating subsection of a pdf
-        with doc.create(Subsection("Table of something")):
-            with doc.create(Tabular("rc|cl")) as table:
-                table.add_hline()
-                table.add_row((1, 2, 3, 4))
-                table.add_hline(1, 2)
-                table.add_empty_row()
-                table.add_row((4, 5, 6, 7))
+    qsect = Section("Shannon Capacity Formula")
+    asect = Section("Shannon Capacity Formula")
+    qdoc.append(qsect)
+    adoc.append(asect)
+    ShannonSolveC(qsect, asect, {}, 0)
 
     # making a pdf using .generate_pdf
     if not os.path.isdir("./build"):
         os.mkdir("./build")
-    doc.generate_pdf("./build/full", clean_tex=False)
+    qdoc.generate_pdf("./build/questions", clean_tex=True)
+    adoc.generate_pdf("./build/answers", clean_tex=True)
